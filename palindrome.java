c94fac5196ef3190d8b72c9d69f53f1bd9c71650
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-01-10 00:32:15 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class palindrome{
+
+     public static void main(String []args){
+        int r,sum=0,temp;
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        temp=n;
+        while(n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+     }
+}
