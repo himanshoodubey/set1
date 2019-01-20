115a6b8a69b0392718c265fab7b5c5557bbb9294
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-01-20 23:01:14 +0530

    Create mutl.java

---
+++ mutl.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int sum=1;
+		for(int i=1;i<=5;i++){
+		    sum=n*i;
+		    	System.out.println(sum); 
+		}
+	
+	}
+}
