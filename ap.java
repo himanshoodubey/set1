071da7a2a77e2e49dd8097b44887603bfa9357b6
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-01-21 22:28:17 +0530

    Create ap.java

---
+++ ap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a=s.nextInt();
+		int d=s.nextInt();
+		int sum=0;
+		for(int i=0;i<n;i++){
+		    sum=sum+a;
+		    a=a+d;
+		   
+		}
+		 System.out.println(sum);
+	}
+}
