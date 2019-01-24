791fdee1d704f12323fe9d7cace2f8f0420902e6
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-01-25 00:45:32 +0530

    Create inc.java

---
+++ inc.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	   
+	    System.out.println(n+1);
+	    
+	}
+}
