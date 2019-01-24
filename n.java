e187bdaafc4cfb9495bf84b2a55aa331b949b14c
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-01-24 23:30:26 +0530

    Create n.java

---
+++ n.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String n=s.nextLine();
+		int m=s.nextInt();
+		for(int i=0;i<m;i++){
+		    System.out.println(n);
+		}
+	}
+}
