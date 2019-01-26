4f6bf8a9e4f447a98a0b53a7c0c2ce0130558a0e
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-01-26 17:41:35 +0530

    Create sum1.java

---
+++ sum1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int m=s.nextInt();
+int sum=0;
+    sum=m+n;
+    System.out.println(sum);
+	}
+}
