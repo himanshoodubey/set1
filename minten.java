8a66052ea7efd8e63fcc9ba8f8ce10052e492ea0
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-01-30 01:05:28 +0530

    Create minten.java

---
+++ minten.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int min=Integer.MAX_VALUE;
+		for(int i=1;i<=10;i++){
+		    int a=s.nextInt();
+		
+		
+		 if(min>a){
+		        min=a;
+		        
+		    }
+	}
+	System.out.println(min);
+}
+}
