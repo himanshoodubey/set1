b6f73780888d29472981e28370f705e02204fcb5
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-02-14 19:30:11 +0530

    Create pointinc.java

---
+++ pointinc.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		
+		double a=s.nextDouble();
+	    int i=(int)a;
+	    
+		System.out.println(++i);
+	}
+}
