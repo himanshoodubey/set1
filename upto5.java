c2c85eef7f9db0a7b7f9ae185962a409698571fc
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-02-23 01:20:31 +0530

    Create upto5.java

---
+++ upto5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		float n=s.nextFloat();
+		float m=s.nextFloat();
+		float area=n*m;
+		System.out.format("%.5f",area);
+	}
+}
