07868517d68b69bf2909e12a2236006a0eb30741
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-04-12 01:16:24 +0530

    Create kelvin.java

---
+++ kelvin.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		n+=273;
+		System.out.print(n);
+	    
+	}
+}
