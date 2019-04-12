b6af413222bdc586078196244f2bdef29c663480
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-04-13 01:29:03 +0530

    Create a*bmodc.java

---
+++ a*bmodc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		int d=a*b;
+		int e=d%c;
+		System.out.print(e);
+		
+}
+}
