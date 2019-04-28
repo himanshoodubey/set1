5351d4f57f91468acf82722c13154ee9d59d4807
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-04-29 00:58:31 +0530

    Create isogram.java

---
+++ isogram.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.Arrays;
+public class Main{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        String a[]=s1.split("");
+        int f=0;
+        for(int i=0;i<s1.length();i++){
+            int c=0;
+            for(int j=0;j<s1.length();j++){
+                
+            
+        
+        
+            if(a[i].equals(a[j])){
+               c++;
+            }
+            
+            
+        }
+        if(c>1)
+        f=1;
+        }
+        if(f==1){
+            System.out.print("no");
+        }
+        else{
+            System.out.print("yes");
+        }
+        
+    }
+}
