0d88f298d59f37ae4d228c22c0e7385fa0604c14
himanshoodubey <44363236+himanshoodubey@users.noreply.github.com>
2019-06-08 21:32:34 +0530

    Create 101.java

---
+++ 101.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+	    String s1=s.nextLine();
+	    int n=s.nextInt();
+	    char a[];
+	    int i=0;
+	        a=s1.toCharArray();
+	    
+	    for(i=a.length;i>a.length-n;i--){
+	        
+	    }
+	    for(;i<a.length;i++){
+	        System.out.print(a[i]);
+	    }
+	    
+	    
+}
+}
